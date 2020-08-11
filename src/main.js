08182973d207132e8369521a8c3ac650b278cb3e
greenthings <69499549+greenthings@users.noreply.github.com>
2020-08-11 16:13:43 +0900

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,11 @@
+var nowZoom = 80;
+
+window.onload = function zooms() {
+    document.body.style.zoom = nowZoom + "%";
+    body.style.webkitTransform = "";
+    body.style.webkitTransformOrigin = "";
+    body.style.mozTransform = "";
+    body.style.mozTransformOrigin = "";
+    body.style.oTransform = "";
+    body.style.oTransformOrigin = "";
+};
